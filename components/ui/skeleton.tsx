9ef1af2bf6193d68780070d26bc64f873e0ea9d8
Awanish Srivastava <awanish@Awanishs-MacBook-Pro.local>
2026-08-30 22:50:53 +0200

    Migrate to Supabase-backed rebuild

--- components/ui/skeleton.tsx
+++
@@ -1,7 +0,0 @@
-import { cn } from "@/lib/utils";
-
-function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn("skeleton rounded-xl", className)} {...props} />;
-}
-
-export { Skeleton };
